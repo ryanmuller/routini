cd8af4d7293702b84d23318a6c911a76eb1a085d
Ryan Muller <cognitionmachine@gmail.com>
2011-11-05 22:08:32 -0400

    adding points controller, needs taskrole association

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,10 +1,25 @@
 class PointsController < ApplicationController
 
   def create 
-    @point = Point.new(params[:point])
-    @point.user = current_user
+    @task = Task.find_by_id(params[:point][:task_id])
+    task_roles = TaskRole.where("task_id = ? AND user_id = ?",
+                                @task.id,
+                                current_user.id)
 
-    @point.save
-    redirect_to tasks_path
+    msg = "Earned"
+
+    task_roles.each do |tr|
+      point = Point.new(params[:point])
+      point.user = current_user
+      point.role = tr.role
+      point.points = tr.points
+      if point.save
+        msg += " +#{tr.points} for #{tr.role}!"
+      end
+    end
+
+    msg = "" if msg == "Earned"
+
+    redirect_to tasks_path, :notice => msg
   end
 end
