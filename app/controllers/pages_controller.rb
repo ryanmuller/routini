6d32c3826f950f252f456a9a6f215d3ea6741d6a
Ryan Muller <cognitionmachine@gmail.com>
2012-04-03 23:38:05 -0400

    removed pages index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,20 +2,6 @@ class PagesController < ApplicationController
   
   before_filter :must_log_in
   
-  def index
-
-    @tasks = current_user.tasks.includes(:task_contexts => :situation, :task_roles => :role)
-    @contexts = current_user.situations
-    @context = current_user.situations.first
-
-    @task = Task.new
-
-    @contexts.each do |context|
-      @task.task_contexts.build(:situation_id => context.id)
-    end
-
-  end
-
   def help
   end
 
